f06b317c0fe13fe209605ab100bc2eb18b228d88
Randall Degges <r@rdegges.com>
2014-09-28 14:06:21 -0500

    Implementing a `toUnderscore` function.

--- index.js
+++ index.js
@@ -58,3 +58,47 @@ String.prototype.toCamelCase = function() {
 
   return modified;
 };
+
+/**
+ * This function returns an underscored version of the string.
+ *
+ * @method
+ * @public
+ *
+ * @returns An underscored version of this string.
+ */
+String.prototype.toUnderscore = function() {
+  var str = this;
+  var modified = '';
+
+  for (var i = 0; i < str.length; i++) {
+
+    // If this character is lowercase, append it to the string.
+    if (isLower(str[i])) {
+      modified += str[i];
+
+    // If this character is uppercase, continue processing.
+    } else {
+
+      // If there is a previous character, continue processing.
+      if (str.length > i-1) {
+
+        // If the previous character is lowercase, then we need to append an
+        // underscore.
+        if (isLower(str[i-1])) {
+          modified += '_' + str[i].toLowerCase();
+        } else {
+          modified += str[i].toLowerCase();
+        }
+
+      // If there's no previous character, then just lowercase this one.
+      } else {
+        modified += str[i].toLowerCase();
+      }
+
+    }
+
+  }
+
+  return modified;
+};
